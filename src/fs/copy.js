70d5f5c3627ae4020cda5597c2710893dd7c7f9a
qobulovasror <qobulovasror0@gmail.com>
2024-01-27 00:08:57 +0500

    fs task completed

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,40 @@
+import { readdir, mkdir, copyFile } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    readdir(__dirname, (err, files) => {
+      if (err) {
+        throw new err;
+      }
+      if (files.includes("files") && !files.includes("files_copy")) {
+        //create files_copy directory
+        mkdir(__dirname+"/files_copy/", err => {
+            if (err){
+                throw err;
+            }
+        })
+
+        //read and copy files
+        readdir(__dirname+"/files/", (err, files) => {
+            if (err) throw err;
+            for(let i=0;i<files.length;i++) {
+                let source = path.join(__dirname + "/files/" + files[i]);
+                let destination = path.join(__dirname + '/files_copy/' + files[i]);
+                copyFile(source, destination, err=>{
+                    if(err) throw err;
+                });
+            }
+        })
+      } else {
+        throw new Error("file already created or file not found");
+      }
+    });
+  } catch (error) {
+    console.log(`FS operation failed: ${error}`);
+  }
 };
 
 await copy();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import {readFile, readdir} from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url)); 
+
 const read = async () => {
-    // Write your code here 
+    try {
+        readFile(__dirname+"/files/fileToRead.txt", {encoding: "utf-8"}, (err, data) => {
+            if(err) throw err;
+            console.log("file contents : \n" + data);
+        })
+    } catch (error) {
+        console.error(`FS operation failed: ${error}`);
+    }
 };
 
 await read();
