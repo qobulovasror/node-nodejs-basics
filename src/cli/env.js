d8ac8b59629e2231b2d7c8cc2174a41efd0ad01c
qobulovasror <qobulovasror0@gmail.com>
2024-01-27 00:14:16 +0500

    cli task completed

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+import process from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+    try {
+        const envs = Object.entries(process.env).filter((e) => e[0].startsWith("RSS_"));
+        envs.forEach(e=>{
+            process.stdout.write(e[0]+"="+e[1]+"; ")
+        })
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 parseEnv();
