68d6ac6e0c0b722e58e02b690cf91873b6efe522
qobulovasror <qobulovasror0@gmail.com>
2024-01-27 22:00:58 +0500

    stream task complated

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import {Transform} from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    const reverseTransform = new Transform({
+      transform(chunk, encoding, callback) {
+        const reversedChunk = chunk.toString().split("").reverse().join("");
+        this.push(reversedChunk);
+        callback();
+      },
+    });
+
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import process from "node:process";
+import { createWriteStream } from "node:fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+  try {
+    const file = createWriteStream(__dirname + "/files/fileToWrite.txt");
+    process.stdin.pipe(file);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await write();
