afd9643e831f1195d38882483368878e46b05acf
qobulovasror <qobulovasror0@gmail.com>
2024-01-29 14:36:23 +0500

    Child Processes task complated

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+import { spawn } from 'child_process';
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [__dirname+'/files/script.js', ...args], { stdio: ['pipe', 'pipe', 'pipe', 'ipc'] });
+    
+    process.stdin.on('data', (chunk) => {
+        child.stdin.write(chunk);
+    });
+    
+    child.stdout.on('data', (data) => {
+        process.stdout.write(data);
+    });
+    
+    child.on('message', (message) => {
+        console.log('Received message from child process:', message);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
