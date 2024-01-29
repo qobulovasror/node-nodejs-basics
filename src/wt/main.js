d7cb51a9d8ed2760f5b95136e0d54955baf87615
qobulovasror <qobulovasror0@gmail.com>
2024-01-29 14:06:07 +0500

    Worker Threads task complated

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker, isMainThread, parentPort, workerData }from 'worker_threads';
+import os from 'os';
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
+const numCPUs = os.cpus().length;
+
+const workerResults = [];
+let completedWorkers = 0;
+
 const performCalculations = async () => {
-    // Write your code here
+    const createWorker = (data) => {
+        const worker = new Worker(__dirname+'/worker.js', { workerData: data });
+        worker.on('message', (result) => {
+            workerResults.push({ status: 'resolved', data: result });
+            completedWorkers++;
+            if (completedWorkers === numCPUs) {
+                console.log(workerResults);
+            }
+        });
+        worker.on('error', (error) => {
+            workerResults.push({ status: 'error', data: null });
+            console.log(error);
+            completedWorkers++;
+            if (completedWorkers === numCPUs) {
+                console.log(workerResults);
+            }
+        });
+    };
+    
+    for (let i = 0; i < numCPUs; i++) {
+        createWorker(10 + i);
+    }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,27 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import {
+  Worker,
+  isMainThread,
+  parentPort,
+  workerData,
+} from "node:worker_threads";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+// n should be received from main thread
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+const sendResult = (n) => {
+    if (isMainThread) {
+        console.log("this is main thread");
+        const worker = new Worker(__filename, { workerData: n });
+        worker.on('message', (result) => {
+            console.log('Result:', result);
+        });
+    } else {
+        const result = nthFibonacci(workerData);
+        parentPort.postMessage(result);
+    }
 };
 
-sendResult();
+sendResult(10);
